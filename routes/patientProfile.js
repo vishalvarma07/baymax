0970f9730825d3c24d88e8ef862b9acdd8b72357
Vishal Varma <55092724+vishalvarma07@users.noreply.github.com>
2022-11-29 16:13:20 -0600

    profile get and post

---
+++ routes/patientProfile.js
@@ -0,0 +1,45 @@
+let express = require('express');
+let router = express.Router();
+let pool = require('../resources');
+
+let credentialCheck = require('../services/credentialCheck');
+
+function validatePhoneNumber(input_str) {
+    var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+  
+    return re.test(input_str);
+  }
+
+router.get('/', credentialCheck, (req, res) => {
+    let userDetails = req.headers;
+    let details = {}
+    pool.query('select * from patient where uname = ',[userDetails.uname], function(err, rows, fields){
+        if(err){
+            console.log(err);
+            details.status = 'failed';
+            res.status(404).json(details);
+        }
+        details.data = rows;
+        res.status(200).json(details);
+    })
+})
+
+router.post('/', credentialCheck, (req, res) => {
+    let userHeaders = req.headers;
+    let userDetails = req.body;
+    let details = {}
+    if(validatePhoneNumber(userDetails.phno)){
+        pool.query('update patient set fName = ?, lName = ?, phno = ?, gender = ?, height = ?, weight = ?, bloodType = ?, apartmentNo = ?, streetName = ?, pincode = ?',[userDetails.fName, userDetails.lName, userDetails.phno, userDetails.gender, userDetails.height, userDetails.weight, userDetails.bloodType, userDetails.apartmentNo, userDetails.streetName, userDetails.pincode], function(err){
+            if(err){
+                console.log(err);
+                details.status = 'failed';
+                res.status(404).json(details);
+            }
+            res.status(200).json(details);
+        })
+    }
+    else{
+        details.status = 'failed';
+        res.status(401).json(details);
+    }
+})
