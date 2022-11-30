f70405bfe81ceafc06c9e7a3acc553af35eab2e9
Sashank <sashankvisweshwaran@gmail.com>
2022-11-30 17:09:03 -0600

    we

--- routes/login.js
+++ routes/login.js
@@ -40,15 +40,14 @@ router.post('/', (req, res) => {
                 details.status = 'failed';
                 res.status(404).json(details);
             }
-            else if(rows.length == 0){
+            if(rows.length == 0){
                 details.status = 'failed';
                 res.status(401).json(details);
             }
             else{
-                const currentYear = String(new Date().getFullYear());
-                const currentMonth = String(new Date().getMonth()+1);
-                console.log(currentMonth, currentYear);
-                const key = rows[0].pwd + loginDetails.user_type +currentMonth + currentYear;
+                const currentYear = String(new Date().getFullYear);
+                const currentMonth = String(new Date().getMonth);
+                const key = rows[0].pwd + currentMonth + currentYear;
                 const hash = createHash('sha256').update(key).digest('hex');
                 if(hash == req.body.hash){
                     details.hash = rows[0].pwd;
