6a22349eade39a5b77a46efea64c4522622cb151
Vishal Varma <55092724+vishalvarma07@users.noreply.github.com>
2022-11-30 15:02:23 -0600

    signup update

--- routes/signup.js
+++ routes/signup.js
@@ -6,11 +6,12 @@ let pool = require('../resources');
 router.post('/', (req, res) => {
     let details = {}
     let signupDetails = req.body;
-    pool.query(`insert into patient (fName, lName, email, pwd, dob) values (?, ?, ?, ?, ?)`,[signupDetails.fName, signupDetails.lName, signupDetails.uname, signupDetails.pwd, signupDetails.dob], function(err){
+    pool.query(`insert into patient (fName, lName, uname, pwd, dob) values (?, ?, ?, ?, ?)`,[signupDetails.fName, signupDetails.lName, signupDetails.uname, signupDetails.pwd, signupDetails.dob], function(err){
         if(err){
             console.log(err);
             details.status = 'failed';
             res.status(404).json(details);
+            return;
         }
         details.status = 'successful';
         res.status(200).json(details);
